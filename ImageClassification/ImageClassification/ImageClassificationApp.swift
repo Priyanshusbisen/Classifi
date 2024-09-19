1192fbd247842c85042339e8bf21eacb68c15b1a
priyanshusbisen <priyanshusbisen@outlook.com>
2024-09-19 17:17:19 -0400

    Initial Commit

---
+++ ImageClassification/ImageClassification/ImageClassificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageClassificationApp.swift
+//  ImageClassification
+//
+//  Created by Priyanshu Singh on 9/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageClassificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ImageClassification/ImageClassificationTests/ImageClassificationTests.swift
@@ -0,0 +1,17 @@
+//
+//  ImageClassificationTests.swift
+//  ImageClassificationTests
+//
+//  Created by Priyanshu Singh on 9/19/24.
+//
+
+import Testing
+@testable import ImageClassification
+
+struct ImageClassificationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
